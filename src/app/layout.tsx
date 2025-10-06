5cae29d1d1469de16b9b06f22cf43539cb244517
paulo-azul <paulotelesserraazul@gmail.com>
2025-10-06 18:21:15 -0300

    continuação da home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  variable: "--font-inter",
+});
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
 "use client"
 
 import "./Page.css";
+import icone_pesquisa from './img/icone_pesquisa.png';
 import Image from 'next/image';
 import Link from "next/link";
 import logo_meuparque from './img/Icon.png';
+import parque_cidade from './img/parque_da_cidade.jpg';
 import { useRef } from 'react';
+import { useState, useEffect } from 'react';
+
 
 export default function Home() {
+
+  const [isScrolled, setIsScrolled] = useState(false)
+  let qtd_usuários = 0
+  let qtd_parques = 0
+  let eventos_menais = 0
+
+
+   useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
 
    const sectionRefs = {
       parques: useRef<HTMLElement>(null),
@@ -17,7 +44,8 @@ export default function Home() {
 
   return (
     <div >
-      <header className="header_pag">
+     
+      <header id="main-header" className={`header_pag ${isScrolled ? 'scrolled' : ''}`}>
         <div className="div_icone">
           <Image src={logo_meuparque} alt="logo MeuParque" className="img_icone"></Image>
           <h1>MeuParque</h1>
@@ -30,9 +58,37 @@ export default function Home() {
         </div>
       </header>
       <div className="pag_home">
-       <h1>teste</h1>      
+        <div className="texto_home">
+          <h1 className="tit_home">Descubra Novos Espaços!</h1>
+          <h2 className="desc_home">Explores parques na sua região! descubra e veja informações de eventos e atividades para toda família! </h2>
+          <div className="div_pesquisa_home">
+            <div className="pesquisa_home">
+              <Image src={icone_pesquisa} alt="icone pesquisa" className="icone_pesquisa_home"></Image>
+              <input type="text" placeholder="Pesquise Parques pelo nome ou localização..." className="inp_pesq"></input>
+            </div>
+            <input type="submit" placeholder="Buscar" className="subm_pesq_home"></input>
+          </div>
+          <div className="dados">
+            <div>
+              <p className="tit_dados">{qtd_parques}+</p>
+              <p className="text_dados">Parques listados</p>
+            </div><div>
+              <p className="tit_dados">{eventos_menais}+</p>
+              <p className="text_dados">Eventos mensais</p>
+            </div><div>
+              <p className="tit_dados">{qtd_usuários}+</p>
+              <p className="text_dados">Usuários ativos</p>
+            </div>
+          </div>
+       </div>
+       <Image src={parque_cidade} alt="imagem do parque da cidade SSA" title="VUMBORA MEU POVO!" className="img1_home"></Image>
       </div>
-      <section ref={sectionRefs.parques}></section>
+      <section ref={sectionRefs.parques}>
+        <div className="txt_parques">
+          <h2 className="tit_parques">Conheça nossos parques:</h2>
+          <h3 className="desc_parque">Descubra o parque do seu próximo rolê, corrida ou passeio em família. Desde dança, academias e até grupos religiosos, os parques estão aqui para todos!</h3>
+        </div>
+      </section>
       <section ref={sectionRefs.eventos}></section>
     </div>
   );
