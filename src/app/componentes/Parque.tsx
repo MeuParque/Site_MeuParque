4b2e0be2d9b037404078e2da943f37dc8a5f9e81
paulo-azul <paulotelesserraazul@gmail.com>
2025-10-10 14:13:12 -0300

    Correção componente nossa missão

--- src/app/componentes/Parque.tsx
+++ src/app/componentes/Parque.tsx
@@ -10,7 +10,6 @@ interface ParqueProps {
 export default function Componentes_Parque({ img_src, img_alt, img_title }: ParqueProps) {
     return (
         <div className='div_componente_parque'>
-            {/* 3. Use as props diretamente */}
             <img className='img_componente_parque' src={img_src} alt={img_alt} title={img_title} />
         </div>
     );
