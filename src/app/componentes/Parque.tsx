239aa6de90e870c077475286a773e3c5aed15490
paulo-azul <paulotelesserraazul@gmail.com>
2025-10-10 13:54:38 -0300

    Correção componente nossa missão

--- src/app/componentes/Parque.tsx
+++ src/app/componentes/Parque.tsx
@@ -1,14 +1,17 @@
 import './Parque.css'
 
-export default function Componentes_Parque(props){
-    return(
+
+interface ParqueProps {
+  img_src: string;
+  img_alt: string;
+  img_title: string;
+}
+
+export default function Componentes_Parque({ img_src, img_alt, img_title }: ParqueProps) {
+    return (
         <div className='div_componente_parque'>
-            <img className='img_componente_parque' src={props.img_src} alt={props.img_alt}  title={props.img_title} />
-            <div className='div_info_parque'>
-                <h1 className='Nome_parque'>{props.nome_parque}</h1>
-                <address></address>
-                <p className='bairro_parque'>{props.descricao} </p>
-            </div>
+            {/* 3. Use as props diretamente */}
+            <img className='img_componente_parque' src={img_src} alt={img_alt} title={img_title} />
         </div>
-    )
+    );
 }
