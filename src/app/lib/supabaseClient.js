c6f145cc4596c2ea3ded97bdcbbe7e3a51fe96c1
paulo-azul <paulotelesserraazul@gmail.com>
2025-11-13 15:10:17 -0300

    Criando Adm usr

--- src/app/lib/supabaseClient.js
+++ src/app/lib/supabaseClient.js
@@ -2,5 +2,16 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+if (!supabaseServiceKey) {
+  console.log('AVISO: Chave de serviço (SUPABASE_SERVICE_ROLE_KEY) não encontrada. As Server Actions podem falhar.');
+}
+
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false
+  }
+});
